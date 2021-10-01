975cc95db9e9547e060de16f9b0555cac26c07e2
囧囧JOJO <58727904+jiongjiongJOJO@users.noreply.github.com>
2021-10-01 20:40:19 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,4 +1,8 @@
-wget -O ~/CentOS-Bash.repo https://raw.githubusercontent.com/jiongjiongJOJO/centos6_yum/main/CentOS-Base.repo
-mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
-cp ~/CentOS-Bash.repo /etc/yum.repos.d/CentOS-Base.repo
-echo "修改yum源完成\n作者：囧囧JOJO"
+curl -o ~/CentOS-Bash.repo https://www.xmpan.com/Centos-6-Vault-Aliyun.repo
+mv -f /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
+cp -f ~/CentOS-Bash.repo /etc/yum.repos.d/CentOS-Base.repo
+yum clean all
+yum makecache
+rm -f ~/CentOS-Bash.repo
+echo "修改yum源完成"
+echo "作者：囧囧JOJO"
