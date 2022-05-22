a5cfd16daeea55dd966664f8dceac94fe57ce023
囧囧JOJO <58727904+jiongjiongJOJO@users.noreply.github.com>
2022-05-22 19:20:53 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
-curl -o ~/CentOS-Bash.repo https://www.xmpan.com/Centos-6-Vault-Aliyun.repo
+curl -k --tlsv1 -o ~/CentOS-Bash.repo https://raw.githubusercontent.com/jiongjiongJOJO/centos6_yum/main/Centos-6-Vault-Aliyun.repo
 mv -f /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
 cp -f ~/CentOS-Bash.repo /etc/yum.repos.d/CentOS-Base.repo
 yum clean all
